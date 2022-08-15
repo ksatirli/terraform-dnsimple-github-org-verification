ecb14a57e096a06ee3c3c253cd6777f07cc10562
Kerim Satirli <k.satirli@gmail.com>
2022-08-15 14:30:35 +0200

    linting

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,7 +2,12 @@
 module "github_org_verification_record" {
   source = "../.."
 
-  domain       = "workloads.io"
-  organization = "workloadsTEST"
+  # This is the DNSimple Domain to create the DNS Record in.
+  domain       = "howdns.works"
+
+  # This is the GitHub Organization slug
+  organization = "howdnsworks"
+
+  # This is the GitHub-provided challenge string
   challenge    = "87e8e461a8"
 }
